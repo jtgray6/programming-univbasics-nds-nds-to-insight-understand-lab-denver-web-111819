aa95b1c85e2c79f9119968e8f4b412ac56f073e4
Jeremy Gray <jeremygray@utexas.edu>
2019-11-16 21:24:42 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,20 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp directors_database
 end
 
 def print_first_directors_movie_titles
+row=0
+column=0
+  while row<directors_database.length
+    if directors_database[row]="Spielberg"
+      while column<directors_database[row].length
+        new=directors_database[row][column]
+      end
+    end
+    column += 1
+  end
+  row += 1
+  pp new
 end
